f28307749b54fde43b7b45772ce8f1fd5f7e28b9
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-10-15 06:23:21 +0000

    [Add]フラッシュメッセージ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,13 +12,22 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
+    redirect_to book_path(book.id)
+  end
+
   def create
     book = Book.new(book_params)
     book.save
+    flash[:notice] = "You have created book successfully."
     redirect_to book_path(book.id)
   end
 
   private
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'books' => 'books#index', as: 'books'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   root to: 'homes#top'
   post 'books' => 'books#create'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 end
