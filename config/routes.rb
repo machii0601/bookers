7abe66bdd64446aa8bff261d6783975a428cecaa
machii0601 <144580610+machii0601@users.noreply.github.com>
2023-10-15 10:22:25 +0000

    [Finish]indexの直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index', as: 'books'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   root to: 'homes#top'
-  post 'books' => 'books#create'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'book#destroy', as: 'destroy_book'
+  resources :books
+
 end
